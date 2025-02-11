898d3606616676153d056f5a0fc58e7ad5dac367
Makyra Kim <makyra20412@gmail.com>
2025-02-10 18:53:35 -0600

    gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MyApp',
+    'posts',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
